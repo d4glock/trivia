affab5da1e0a4c918b29d4fcb607c84ee4480b1b
d4glock <diegomesinaes@gmail.com>
2024-12-15 02:58:23 -0300

    login csss

--- trivia_project/players/views.py
+++ trivia_project/players/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render, redirect    
 from django.contrib.auth.decorators import login_required    
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm    
-from django.contrib.auth import login, authenticate    
+from django.contrib.auth import login, authenticate, logout    
 from django.contrib import messages    
 from .models import Player    
 from rest_framework import viewsets    
 from api.serializers import PlayerSerializer
-from django.contrib.auth import logout
-
-
+from django.core.paginator import Paginator
 
 def register_view(request):    
     if request.method == 'POST':    
@@ -18,7 +16,7 @@ def register_view(request):
             user = form.save()    
             if not Player.objects.filter(user=user).exists():
                 Player.objects.create(user=user)    
-               
+
             username = form.cleaned_data.get('username')    
             raw_password = form.cleaned_data.get('password1')    
             user = authenticate(username=username, password=raw_password)    
@@ -37,7 +35,7 @@ def custom_redirect(request):
         return redirect('/admin/')  # URL del Django Admin
     else:
         return redirect('/')  # URL para usuarios normales
-    
+
 def logout_view(request):
     logout(request)
     messages.success(request, '¡Has cerrado sesión exitosamente!')
@@ -54,7 +52,7 @@ def login_view(request):
                 login(request, user)
                 messages.success(request, f'Bienvenido {username}!')
                 return redirect('custom_redirect')  # Redirige al custom_redirect
-                
+
         else:    
             messages.error(request, 'Usuario o contraseña incorrectos.')    
     else:    
@@ -67,10 +65,14 @@ def profile_view(request):
 
 @login_required    
 def leaderboard_view(request):    
-    players = Player.objects.all().order_by('-total_score')    
-    return render(request, 'players/leaderboard.html', {'players': players})    
+    players_list = Player.objects.all().order_by('-total_score')
+    paginator = Paginator(players_list, 7)  # 7 jugadores por página
 
+    page = request.GET.get('page')
+    players = paginator.get_page(page)
+
+    return render(request, 'players/leaderboard.html', {'players': players})    
 
 class PlayerViewSet(viewsets.ModelViewSet):    
     queryset = Player.objects.all()    
-    serializer_class = PlayerSerializer  
+    serializer_class = PlayerSerializer
